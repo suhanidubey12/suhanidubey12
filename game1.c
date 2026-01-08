9bbbff276d8cb27b662ea0ea4bf2451339153a07
suhanidubey12 <dubeysuhani6@gmail.com>
2026-01-08 17:33:51 +0530

    Add files via upload

---
+++ game1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number, guess, attempts = 0;
+
+    // Seed for random number
+    srand(time(0));
+
+    // Random number between 1 and 100
+    number = rand() % 100 + 1;
+
+    printf("🎮 Welcome to Number Guessing Game!\n");
+    printf("Guess a number between 1 and 100\n");
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess > number) {
+            printf("Too High!\n");
+        } else if (guess < number) {
+            printf("Too Low!\n");
+        } else {
+            printf("🎉 Correct! You guessed it in %d attempts.\n", attempts);
+        }
+
+    } while (guess != number);
+
+    return 0;
+}
