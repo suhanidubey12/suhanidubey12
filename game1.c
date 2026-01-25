4eb42dd27564260fd57d4a4db6e577554e162ee4
suhanidubey12 <dubeysuhani6@gmail.com>
2026-01-25 21:53:03 +0530

    Update game1.c

--- game1.c
+++ game1.c
@@ -9,7 +9,7 @@ int main() {
     srand(time(0));
 https://github.com/suhanidubey12/suhanidubey12/edit/main/game1.c
     // Random number between 1 and 100
-    number = rand() % 100 + 1;
+    number = rand() % 100 + 1
 
     printf("🎮 Welcome to Number Guessing Game!\n")l
     printf("Guess a number between 1 and 100\n");
@@ -31,6 +31,7 @@ https://github.com/suhanidubey12/suhanidubey12/edit/main/game1.c
 
     return 0;
 }
+
 
 
 
