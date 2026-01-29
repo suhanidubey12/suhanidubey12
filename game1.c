dfde1694d5750d06648daace4816838a35fd2671
suhanidubey12 <dubeysuhani6@gmail.com>
2026-01-29 22:42:08 +0530

    Update game1.c

--- game1.c
+++ game1.c
@@ -3,6 +3,7 @@
 #include <time.h> 
 
 
+
 int main() {
     int number, guess, attempts = 0.;
 
@@ -33,6 +34,7 @@ https://github.com/suhanidubey12/suhanidubey12/edit/main/game1.c
 
     return 0;
 }
+
 
 
 
