15423a996a5a19074b8c2ca1ecbcb1804062a90f
suhanidubey12 <dubeysuhani6@gmail.com>
2026-02-10 12:46:45 +0530

    Update game1.c

--- game1.c
+++ game1.c
@@ -9,7 +9,7 @@ int main() {
 
 
     // Seed for random number
-    srand(time(0));
+    srand(time(0))
 https://github.com/suhanidubey12/suhanidubey12/edit/main/game1.c
     // Random number between 1 and 100
     number = rand() % 100 + 1;
@@ -34,6 +34,7 @@ https://github.com/suhanidubey12/suhanidubey12/edit/main/game1.c
 
     return 0;
 }
+
 
 
 
