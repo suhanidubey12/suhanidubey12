3ce49751a1e2db94a5ccc5f959ee3a751fdcfbde
suhanidubey12 <dubeysuhani6@gmail.com>
2026-02-11 07:28:11 +0530

    Update game1.c

--- game1.c
+++ game1.c
@@ -6,7 +6,6 @@ int main() {
     int number, guess, attempts = 0.;
 
     
-
     
 
     // Seed for random number
@@ -35,6 +34,7 @@ https://github.com/suhanidubey12/suhanidubey12/edit/main/game1.c
 
     return 0;
 }
+
 
 
 
